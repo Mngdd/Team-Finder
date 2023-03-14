2eea261a82a191d9ff81c640cf9496bc39781430
xenia <troshkinaksenia@icloud.com>
2023-03-14 19:23:09 +0300

    Login, register and add project unfinished forms

--- forms/user.py
+++ forms/user.py
@@ -1,34 +1,28 @@
 from flask_wtf import FlaskForm
-from wtforms import EmailField, PasswordField, BooleanField, SubmitField, StringField, IntegerField, DateField
+from wtforms import EmailField, PasswordField, BooleanField, SubmitField, StringField, IntegerField, TextAreaField, \
+    SelectMultipleField
 from wtforms.validators import DataRequired
 
 
 class RegisterForm(FlaskForm):
-    email = EmailField('Почта', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
-    surname = StringField('Фамилия пользователя', validators=[DataRequired()])
-    name = StringField('Имя пользователя', validators=[DataRequired()])
-    age = IntegerField('Возраст', validators=[DataRequired()])
-    position = StringField('Позиция', validators=[DataRequired()])
-    specialty = StringField('Специальность', validators=[DataRequired()])
-    address = StringField('Адрес', validators=[DataRequired()])
-    submit = SubmitField('Войти')
+    email = EmailField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password_again = PasswordField('Repeat password', validators=[DataRequired()])
+    nickname = StringField('Nickname', validators=[DataRequired()])
+    github_profile = StringField('Github profile', validators=[DataRequired()])
+    submit = SubmitField('Register')
 
 
 class LoginForm(FlaskForm):
-    email = EmailField('Почта', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    remember_me = BooleanField('Запомнить меня')
-    submit = SubmitField('Войти')
+    email = EmailField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember me')
+    submit = SubmitField('Log in')
 
 
-class JobsForm(FlaskForm):
-    team_leader = StringField('Team leader id', validators=[DataRequired()])
-    job = StringField('Job description', validators=[DataRequired()])
-    work_size = IntegerField('Work size (hours)', validators=[DataRequired()])
-    collaborators = StringField('Collaborators', validators=[DataRequired()])
-    start_date = DateField('Start date', validators=[DataRequired()])
-    end_date = DateField('End date', validators=[DataRequired()])
-    is_finished = BooleanField('Is finished', validators=[DataRequired()])
-    submit = SubmitField('Add job')
+class ProjectForm(FlaskForm):
+    title = StringField('Project title', validators=[DataRequired()])
+    description = TextAreaField('Project description', validators=[DataRequired()])
+    collaborators = IntegerField('Number of collaborators', validators=[DataRequired()])
+    tags = SelectMultipleField('Tags')
+    submit = SubmitField('Add project')
